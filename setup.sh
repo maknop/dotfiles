c6212eff685706b1cd0f155b16ccccdef3cfed13
Matthew Knop <knopmatthew93@gmail.com>
2021-05-09 20:27:53 -0600

    Revert back to echo instead of printf.

--- setup.sh
+++ setup.sh
@@ -19,44 +19,44 @@ WHITE='\033[1;37m'
 echo "${GREEN}Verifying that Git is installed...${WHITE}"
 command -v git > /dev/null 2>&1
 if (( $? != 0 )) ; then
-    echo "${RED}Git is required to update dotfiles 1>&2${WHITE}\n"
+    echo "${RED}Git is required to update dotfiles 1>&2${WHITE}"
     exit 1
 fi
-echo "${GREEN}Git is installed on this device!${WHITE}\n"
+echo "${GREEN}Git is installed on this device!${WHITE}"
 
 # Installing programs based on detected OS.
 if [[ `uname` == "Darwin" ]]; then
     echo "${GREEN}MacOS detected!${WHITE}"
     if [ ! -d "${HOME}/.oh-my-zsh" ]; then
-        echo "    ${GREEN}Installing Oh-My-Zsh as alternative to Bash${WHITE}"
+        echo "${GREEN}Installing Oh-My-Zsh as alternative to Bash${WHITE}"
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     else
-        echo "    ${LIGHTBLUE}Oh-My-Zsh is already installed...${WHITE}\n"
+        echo "${LIGHTBLUE}Oh-My-Zsh is already installed...${WHITE}"
     fi
 
     if [[ ! -d "/Applications/iTerm.app" ]]; then
-        echo "\t${GREEN}Installing iterm2 with Brew${WHITE}\n"
+        echo "${GREEN}Installing iterm2 with Brew${WHITE}"
         brew install iterm2
     else
-        echo "\t${LIGHTBLUE}Iterm2 is already installed...${WHITE}\n"
+        echo "${LIGHTBLUE}Iterm2 is already installed...${WHITE}"
     fi
 
     if [ type tmux >/dev/null 2>/dev/null ]; then
-        echo "\t${GREEN}Installing tmux with Brew${WHITE}\n"
+        echo "${GREEN}Installing tmux with Brew${WHITE}"
         brew install tmux && brew upgrade tmux
     else
-        echo "${LIGHTBLUE}Tmux is already installed...${WHITE}\n"
+        echo "${LIGHTBLUE}Tmux is already installed...${WHITE}"
     fi
 
 elif [[ `uname` == "Linux" ]]; then
     echo "${GREEN}Linux Operating System Detected!"
-    echo "\t${GREEN}Installing Oh-My-Zsh as alternative to Bash${WHITE}"
+    echo "${GREEN}Installing Oh-My-Zsh as alternative to Bash${WHITE}"
     sudo apt-get install zsh
 
-    echo "\t${GREEN}Installing Yakuake${WHITE}"
+    echo "${GREEN}Installing Yakuake${WHITE}"
     sudo apt-get install yakuake
 
-    echo "\t${GREEN}Installing tmux${WHITE}"
+    echo "${GREEN}Installing tmux${WHITE}"
     sudo apt install tmux
 
 else
@@ -68,22 +68,22 @@ echo "\n"
 # Clone dotfiles if they aren't present
 if [ ! -d "$HOME/.dotfiles" ]; then
     # Clone the dotfiles
-    echo "${GREEN}Cloning remote dotfiles...${WHITE}\n"
+    echo "${GREEN}Cloning remote dotfiles...${WHITE}"
     git clone --recursive https://github.com/${repository_location} -b ${repository_branch} $HOME/.dotfiles
     git_exit_status=$?
 fi
 
 # Pull the most updated copy
-echo "${GREEN}Pulling most updated copy of dotfiles...${WHITE}\n"
+echo "${GREEN}Pulling most updated copy of dotfiles...${WHITE}"
 cd $HOME/.dotfiles && git pull --ff-only
 git_exit_status=$?
 
 # If the clone/pull operation failed, exit with the exit status provided by git
 if (( $git_exit_status != 0 )); then
-    echo "${RED}There was an error while attempting to clone/pull dotfiles! 1>&2${WHITE}\n"
+    echo "${RED}There was an error while attempting to clone/pull dotfiles! 1>&2${WHITE}"
     exit $git_exit_status
 elif (( $git_exit_status == 0 )); then
-    echo "${GREEN}Successfully clone/pulled dotfiles!${WHITE}\n" 
+    echo "${GREEN}Successfully clone/pulled dotfiles!${WHITE}" 
 fi
 
 if [ -f ~/.functions ]; then
@@ -91,31 +91,31 @@ if [ -f ~/.functions ]; then
 fi
 
 # Symlink all the files
-echo "${GREEN}Symlinking dotfiles into ${HOME}${WHITE}\n"
+echo "${GREEN}Symlinking dotfiles into ${HOME}${WHITE}"
 ln -sf $HOME/.dotfiles/files/.[!.]* $HOME
 
-echo "${GREEN}Creating a vim directory.${WHITE}\n"
+echo "${GREEN}Creating a vim directory.${WHITE}"
 if [ ! -d "$HOME/.vim" ]; then 
-    echo "${GREEN}Creating a vim folder...${WHITE}\n"
+    echo "${GREEN}Creating a vim folder...${WHITE}"
     mkdir .vim
     mkdir .vim/bundle
 fi
 
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    echo "${GREEN}Setup Vundle for VIM package management${WHITE}\n"
+    echo "${GREEN}Setup Vundle for VIM package management${WHITE}"
 else
-    echo "${LIGHTBLUE}Vundle is already installed...${WHITE}\n"
+    echo "${LIGHTBLUE}Vundle is already installed...${WHITE}"
 fi
 
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    echo "${GREEN}tpm installed...${WHITE}\n"
+    echo "${GREEN}tpm installed...${WHITE}"
 else
-    echo "${LIGHTBLUE}tpm is already installed${WHITE}\n"
+    echo "${LIGHTBLUE}tpm is already installed${WHITE}"
 fi
 
-echo "${GREEN}Installing Vim packages with Vundle...${WHITE}\n\n"
+echo "${GREEN}Installing Vim packages with Vundle...${WHITE}\n"
 vim -c 'PluginInstall' -c 'qa!'
 
 echo "${PURPLE}Finished installing dotfiles. Happy coding!${WHITE}\n"
