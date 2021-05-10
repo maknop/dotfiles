c238e834a8262b67baecdf1218b256475e2f6e9c
Matthew Knop <knopmatthew93@gmail.com>
2021-05-09 20:09:13 -0600

    Revert back to echo instead of printf.

--- setup.sh
+++ setup.sh
@@ -34,52 +34,52 @@ if [[ `uname` == "Darwin" ]]; then
     fi
 
     if [[ ! -d "/Applications/iTerm.app" ]]; then
-        printf "${GREEN}Installing iterm2 with Brew${WHITE}"
+        printf "${GREEN}Installing iterm2 with Brew${WHITE}\n"
         brew install iterm2
     else
-        printf "${LIGHTBLUE}Iterm2 is already installed...${WHITE}"
+        printf "${LIGHTBLUE}Iterm2 is already installed...${WHITE}\n"
     fi
 
     if [ type tmux >/dev/null 2>/dev/null ]; then
-        printf "${GREEN}Installing tmux with Brew${WHITE}"
+        printf "${GREEN}Installing tmux with Brew${WHITE}\n"
         brew install tmux && brew upgrade tmux
     else
-        printf "${LIGHTBLUE}Tmux is already installed...${WHITE}"
+        printf "${LIGHTBLUE}Tmux is already installed...${WHITE}\n"
     fi
 
 elif [[ `uname` == "Linux" ]]; then
-    printf "${GREEN}Installing Oh-My-Zsh as alternative to Bash${WHITE}"
+    printf "${GREEN}Installing Oh-My-Zsh as alternative to Bash${WHITE}\n"
     sudo apt-get install zsh
 
-    printf "${GREEN}Installing Yakuake${WHITE}"
+    printf "${GREEN}Installing Yakuake${WHITE}\n"
     sudo apt-get install yakuake
 
-    printf "${GREEN}Installing tmux${WHITE}"
+    printf "${GREEN}Installing tmux${WHITE}\n"
     sudo apt install tmux
 
 else
-    printf "${RED}Unknown OS!"
+    printf "${RED}Unknown OS!${WHITE}\n"
 fi
 
 # Clone dotfiles if they aren't present
 if [ ! -d "$HOME/.dotfiles" ]; then
     # Clone the dotfiles
-    printf "${GREEN}Cloning remote dotfiles...${WHITE}"
+    printf "${GREEN}Cloning remote dotfiles...${WHITE}\n"
     git clone --recursive https://github.com/${repository_location} -b ${repository_branch} $HOME/.dotfiles
     git_exit_status=$?
 fi
 
 # Pull the most updated copy
-printf "${GREEN}Pulling most updated copy of dotfiles...${WHITE}"
+printf "${GREEN}Pulling most updated copy of dotfiles...${WHITE}\n"
 cd $HOME/.dotfiles && git pull --ff-only
 git_exit_status=$?
 
 # If the clone/pull operation failed, exit with the exit status provided by git
 if (( $git_exit_status != 0 )); then
-    printf "${RED}There was an error while attempting to clone/pull dotfiles! 1>&2${WHITE}"
+    printf "${RED}There was an error while attempting to clone/pull dotfiles! 1>&2${WHITE}\n"
     exit $git_exit_status
 elif (( $git_exit_status == 0 )); then
-    printf "${GREEN}Successfully clone/pulled dotfiles!${WHITE}" 
+    printf "${GREEN}Successfully clone/pulled dotfiles!${WHITE}\n" 
 fi
 
 if [ -f ~/.functions ]; then
@@ -87,31 +87,31 @@ if [ -f ~/.functions ]; then
 fi
 
 # Symlink all the files
-printf "${GREEN}Symlinking dotfiles into ${HOME}${WHITE}"
+printf "${GREEN}Symlinking dotfiles into ${HOME}${WHITE}\n"
 ln -sf $HOME/.dotfiles/files/.[!.]* $HOME
 
-printf "${GREEN}Creating a vim directory.${WHITE}"
+printf "${GREEN}Creating a vim directory.${WHITE}\n"
 if [ ! -d "$HOME/.vim" ]; then 
-    printf "${GREEN}Creating a vim folder...${WHITE}"
+    printf "${GREEN}Creating a vim folder...${WHITE}\n"
     mkdir .vim
     mkdir .vim/bundle
 fi
 
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    printf "${GREEN}Setup Vundle for VIM package management${WHITE}"
+    printf "${GREEN}Setup Vundle for VIM package management${WHITE}\n"
 else
-    printf "${LIGHTBLUE}Vundle is already installed...${WHITE}"
+    printf "${LIGHTBLUE}Vundle is already installed...${WHITE}\n"
 fi
 
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    printf "${GREEN}tpm installed...${WHITE}"
+    printf "${GREEN}tpm installed...${WHITE}\n"
 else
-    printf "${LIGHTBLUE}tpm is already installed${WHITE}"
+    printf "${LIGHTBLUE}tpm is already installed${WHITE}\n"
 fi
 
-printf "${GREEN}Installing Vim packages with Vundle...${WHITE}"
+printf "${GREEN}Installing Vim packages with Vundle...${WHITE}\n\n"
 script -c 'vim' -c 'PluginInstall' -c 'qa!'
 
 printf "${PURPLE}Finished installing dotfiles. Happy coding!${WHITE}"
