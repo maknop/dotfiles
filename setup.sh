59d13aaecdddbc56bd7ad51bf71af912d75f21bc
Matthew Knop <knopmatthew93@gmail.com>
2021-05-09 18:46:33 -0600

    Added more color to text.

--- setup.sh
+++ setup.sh
@@ -13,68 +13,69 @@ GREEN='\033[0;32m'
 LIGHTBLUE='\033[1;34m'
 CYAN='\033[0;36m'
 PURPLE='\033[0;35m'
+WHITE='\033[1;37m'
 
 # Check that git is installed
-echo -e"${GREEN}Verifying that Git is installed..."
+echo "${GREEN}Verifying that Git is installed...${WHITE}"
 command -v git > /dev/null 2>&1
 if (( $? != 0 )) ; then
-    echo -e "${RED}Git is required to update dotfiles 1>&2"
+    echo "${RED}Git is required to update dotfiles 1>&2${WHITE}"
     exit 1
 fi
 
 # Installing programs based on detected OS.
 if [[ `uname` == "Darwin" ]]; then
-    echo -e "${GREEN}Installing Oh-My-Zsh as alternative to Bash"
+    echo "${GREEN}Installing Oh-My-Zsh as alternative to Bash${WHITE}"
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
     if [[ ! -d "/Applications/iTerm.app" ]]; then
-        echo -e "${GREEN}Installing iterm2 with Brew"
+        echo "${GREEN}Installing iterm2 with Brew${WHITE}"
         brew install iterm2
     else
-        echo -e "${LIGHTBLUE}Iterm2 is already installed..."
+        echo "${LIGHTBLUE}Iterm2 is already installed...${WHITE}"
     fi
 
     if [ type tmux >/dev/null 2>/dev/null ]; then
-        echo Installing tmux with Brew
+        echo "${GREEN}Installing tmux with Brew${WHITE}"
         brew install tmux
         brew upgrade tmux
     else
-        echo Tmux is already installed...
+        echo "${LIGHTBLUE}Tmux is already installed...${WHITE}"
     fi
 
 elif [[ `uname` == "Linux" ]]; then
-    echo 'Installing Oh-My-Zsh as alternative to Bash'
+    echo "${GREEN}Installing Oh-My-Zsh as alternative to Bash${WHITE}"
     sudo apt-get install zsh
 
-    echo 'Installing Yakuake'
+    echo "${GREEN}Installing Yakuake${WHITE}"
     sudo apt-get install yakuake
 
-    echo 'Installing tmux'
+    echo "${GREEN}Installing tmux${WHITE}"
     sudo apt install tmux
 
 else
-    echo 'Unknown OS!'
+    echo "${RED}Unknown OS!"
 fi
 
 # Clone dotfiles if they aren't present
 if [ ! -d "$HOME/.dotfiles" ]; then
     # Clone the dotfiles
-    echo Cloning remote dotfiles...
+    echo "${GREEN}Cloning remote dotfiles...${WHITE}"
     git clone --recursive https://github.com/${repository_location} -b ${repository_branch} $HOME/.dotfiles
     git_exit_status=$?
 fi
 
 # Pull the most updated copy
-echo Pulling most updated copy of dotfiles...
+echo "${GREEN}Pulling most updated copy of dotfiles...${WHITE}"
 cd $HOME/.dotfiles && git pull --ff-only
 git_exit_status=$?
 
 # If the clone/pull operation failed, exit with the exit status provided by git
 if (( $git_exit_status != 0 )); then
-    echo There was an error while attempting to clone/pull dotfiles! 1>&2
+    echo "${RED}There was an error while attempting to clone/pull dotfiles! 1>&2${WHITE}"
     exit $git_exit_status
 elif (( $git_exit_status == 0 )); then
-    echo Successfully clone/pulled dotfiles!
+    echo "${GREEN}Successfully clone/pulled dotfiles!${WHITE}" 
 fi
 
 if [ -f ~/.functions ]; then
@@ -82,31 +83,31 @@ if [ -f ~/.functions ]; then
 fi
 
 # Symlink all the files
-echo Symlinking dotfiles into ${HOME}
+echo "${GREEN}Symlinking dotfiles into ${HOME}${WHITE}"
 ln -sf $HOME/.dotfiles/files/.[!.]* $HOME
 
-echo Creating a vim directory.
+echo "${GREEN}Creating a vim directory.${WHITE}"
 if [ ! -d "$HOME/.vim" ]; then 
-    echo Creating a vim folder...
+    echo "${GREEN}Creating a vim folder...${WHITE}"
     mkdir .vim
     mkdir .vim/bundle
 fi
 
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    echo Setup Vundle for VIM package management
+    echo "${GREEN}Setup Vundle for VIM package management${WHITE}"
 else
-    echo Vundle is already installed...
+    echo "${LIGHTBLUE}Vundle is already installed...${WHITE}"
 fi
 
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    echo tpm installed...
+    echo "${GREEN}tpm installed...${WHITE}"
 else
-    echo tpm is already installed
+    echo "${LIGHTBLUE}tpm is already installed${WHITE}"
 fi
 
-echo -e "${GREEN}Installing Vim packages with Vundle..."
+echo "${GREEN}Installing Vim packages with Vundle...${WHITE}"
 vim -c 'PluginInstall' -c 'qa!'
 
-echo "${PURPLE}Finished installing dotfiles. Happy coding!"
+echo "${PURPLE}Finished installing dotfiles. Happy coding!${WHITE}"
