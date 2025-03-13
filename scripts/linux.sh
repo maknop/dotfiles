9a6b02866d05f8ad0b67983e7e21587d15926d70
Matt Knop <knopmatthew93@gmail.com>
2025-03-12 21:41:10 -0600

    more lint fixes

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -3,7 +3,7 @@
 check_linux_distro() {
 
     # ubuntu or fedora
-    LINUX_DISTRO=$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release)
+    local LINUX_DISTRO=$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release)
 
     if [ "$LINUX_DISTRO" == "ubuntu" ]; then
         echo "apt"
@@ -20,23 +20,23 @@ git --version 2>&1 >/dev/null
 IS_GIT_AVAILABLE=$?
 
 if [ "$IS_GIT_AVAILABLE" -eq 0 ]; then
-    $LINUX_PACKAGE_RUNTIME install -y git   
+    sudo "$LINUX_PACKAGE_RUNTIME" install -y git   
 fi
 
 # Install oh-my-zsh
 chsh -s /bin/zsh
-sudo $LINUX_PACKAGE_RUNTIME install -y zsh 
+sudo "$LINUX_PACKAGE_RUNTIME" install -y zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Neovim
-sudo $LINUX_PACKAGE_RUNTIME install -y neovim python3-neovim
+sudo "$LINUX_PACKAGE_RUNTIME" install -y neovim python3-neovim
 
 # Install Plug for Neovim package management
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install Tmux
-sudo $LINUX_PACKAGE_RUNTIME install -y tmux
+sudo "$LINUX_PACKAGE_RUNTIME" install -y tmux
 # Install Tmux Plugin Manager (TPM)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
