ebacef7b41eb424c7b3c8fe54d734893d3ba682f
Matt Knop <knopmatthew93@gmail.com>
2025-03-12 21:38:36 -0600

    fixing lint issues

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -3,41 +3,40 @@
 check_linux_distro() {
 
     # ubuntu or fedora
-    linux_distro=$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release)
+    LINUX_DISTRO=$(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release)
 
-    if [ linux_distro == "ubuntu" ]; then
-        return "apt"
-    elif [ linux_distro == "fedora" ]; then
-        return "dnf"
+    if [ "$LINUX_DISTRO" == "ubuntu" ]; then
+        echo "apt"
+    elif [ "$LINUX_DISTRO" == "fedora" ]; then
+        echo "dnf"
     fi
 
-    return ""
+    echo ""
 }
 
-linux_package_runtime=$(check_linux_distro)
+LINUX_PACKAGE_RUNTIME=$(check_linux_distro)
 
 git --version 2>&1 >/dev/null
 IS_GIT_AVAILABLE=$?
 
-if [ $IS_GIT_AVAILABLE -eq 0 ]; then
-    $linux_package_runtime install -y git   
+if [ "$IS_GIT_AVAILABLE" -eq 0 ]; then
+    $LINUX_PACKAGE_RUNTIME install -y git   
 fi
 
 # Install oh-my-zsh
 chsh -s /bin/zsh
-sudo $linux_package_runtime install -y zsh 
+sudo $LINUX_PACKAGE_RUNTIME install -y zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Neovim
-sudo $linux_package_runtime install -y neovim python3-neovim
+sudo $LINUX_PACKAGE_RUNTIME install -y neovim python3-neovim
 
 # Install Plug for Neovim package management
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install Tmux
-sudo $linux_package_runtime install -y tmux
-
+sudo $LINUX_PACKAGE_RUNTIME install -y tmux
 # Install Tmux Plugin Manager (TPM)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
