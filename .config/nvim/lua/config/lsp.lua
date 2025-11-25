611c4a569366496c1b739f5acba3d04b9d17be18
Matt Knop <mknop@redhat.com>
2025-11-24 22:46:11 -0700

    fixing deprecated lspconfig

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,26 +1,36 @@
 -- LSP Configuration
-local lspconfig = require('lspconfig')
-
 -- Get default capabilities from nvim-cmp
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Golang LSP
-lspconfig.gopls.setup {
+vim.lsp.config.gopls = {
+  cmd = { 'gopls' },
+  filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+  root_markers = { 'go.work', 'go.mod', '.git' },
   capabilities = capabilities,
 }
 
 -- Python LSP - Ruff (Might need to run pip install ruff-lsp)
-lspconfig.ruff.setup {
+vim.lsp.config.ruff = {
+  cmd = { 'ruff', 'server' },
+  filetypes = { 'python' },
+  root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', '.git' },
   capabilities = capabilities,
 }
 
 -- Python LSP - Pyright (Might need to pip install pyright)
-lspconfig.pyright.setup {
+vim.lsp.config.pyright = {
+  cmd = { 'pyright-langserver', '--stdio' },
+  filetypes = { 'python' },
+  root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', '.git' },
   capabilities = capabilities,
 }
 
 -- TypeScript LSP (Might need to run npm install -g typescript typescript-language-server)
-lspconfig.ts_ls.setup {
+vim.lsp.config.ts_ls = {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { 'javascript', 'typescript', 'typescriptreact', 'typescript.tsx' },
+  root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' },
   capabilities = capabilities,
   init_options = {
     plugins = {
@@ -31,10 +41,10 @@ lspconfig.ts_ls.setup {
       },
     },
   },
-  filetypes = {
-    "javascript",
-    "typescript",
-    "typescriptreact",
-    "typescript.tsx"
-  },
 }
+
+-- Enable LSP servers
+vim.lsp.enable('gopls')
+vim.lsp.enable('ruff')
+vim.lsp.enable('pyright')
+vim.lsp.enable('ts_ls')
